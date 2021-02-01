eeca97a750f9558cf570b9d466208155c59017ec
it17150644 <it17150644@my.sliit.lk>
2021-02-01 23:05:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine'
+import './App.css'
+import ChatFeed from './Components/ChatFeed';
+import LoginForm from './Components/LoginForm';
+// add 3 dependencies -   npm install @ant-design/icons axios react-chat-engine 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="d50bf182-50d5-4f67-a2bf-2a5faf66318d"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+         />
+    );
 }
 
 export default App;
---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = {
+      "Project-ID": "d50bf182-50d5-4f67-a2bf-2a5faf66318d",
+      "User-Name": username,
+      "User-Secret": password,
+    };
+
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject
+      });
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      window.location.reload();
+    } catch (error) {
+      setError("Oops, Incorrect Credentials !!!");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <center>
+        <p><b>Login with this Credentials</b> <br/>(Username : <i>guest</i> || Password : <i>guest</i>) </p>
+        <br/>
+        </center>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start Chatting</span>
+            </button>
+          </div>
+        </form>
+        <h2 className="error">{error}</h2>
+      </div>
+    </div>
+    
+  );
+};
+
+export default LoginForm;
